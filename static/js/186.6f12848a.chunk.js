74dfbb1d1647e58a75a8754fd004c01a16d16b48
AntonPlish <95130662+AntonPlish@users.noreply.github.com>
2022-09-21 15:46:01 +0000

    Deploying to gh-pages from @ AntonPlish/goit-react-hw-05-movies@059bb5cc3e4885ad95a2e5a765407bd9b22c84d6 🚀

---
+++ static/js/186.6f12848a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[186],{186:function(e,i,n){n.r(i),n.d(i,{Reviews:function(){return s}});var r=n(184),s=function(){return(0,r.jsx)("main",{children:(0,r.jsx)("h1",{children:"Movies page"})})}}}]);
+//# sourceMappingURL=186.6f12848a.chunk.js.map
