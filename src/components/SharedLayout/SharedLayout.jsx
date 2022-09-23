775218f802aa55cf2da74ed448a1d16ad99130ff
AntonPlish <antonplish@gmail.com>
2022-09-23 10:59:56 +0300

    Commit

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,13 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Container, Header, Link } from "../SharedLayout.styled";
+import { Container, Header, Link } from "./SharedLayout.styled";
 
 export const SharedLayout = () => {
     return (
         <Container>
             <Header>
                 <nav>
-                    <Link to="/">Home</Link>
+                    <Link to="/home">Home</Link>
                     <Link to="/movies">Movies</Link>
                 </nav>
             </Header>
